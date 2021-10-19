ce9557abb98a421eb3432e43d3df26e7588f2f53
fahmihend <fahmihendriawan18@gmail.com>
2021-10-19 17:37:41 +0700

    layout

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -2,6 +2,6 @@ import styles from '../footer/Footer.module.css';
 
 export default function Footer() {
     return (
-        <p className={styles.title}>B a ™ a N</p>
+        <p className={styles.title}>Made By B a ™ a N</p>
     )
 }
