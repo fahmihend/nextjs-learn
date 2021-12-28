7c158fb36a67e9473e4ce93a3e2340aa92e3c6d2
fahmihend <fahmihendriawan18@gmail.com>
2021-12-28 14:25:59 +0700

    data fetching

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,9 +1,34 @@
 import Layout from "../components/layout";
+import styles from "../styles/Blog.module.css"
+interface Post {
+  id: number;
+  tittle: string;
+  body: string;
+}
 
-export default function Blog() {
+interface BlogProps{
+  dataBlog: Post[]
+}
+export default function Blog(props:BlogProps) {
+  const {dataBlog} = props 
   return (
     <Layout pageTitle="Blog Page">
-      <h1 className={"title-homepage"}>Blog Page</h1>
+      {dataBlog.map((blog) => (
+        <div key={blog.id} className={styles.card}>
+          <h3>{blog.tittle}</h3>
+          <p>{blog.body}</p>
+        </div>
+      ))}
     </Layout>
   );
+}
+
+export async function getServerSideProps() {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+  const dataBlog = await res.json();
+  return {
+    props: {
+      dataBlog,
+    },
+  };
 }
--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -1,9 +1,36 @@
+import { useRouter } from "next/router";
 import Layout from "../../components/layout";
+import styles from "../../styles/Users.module.css"
 
-export default function user() {
+interface UserProps {
+    dataUsers: Array<any>;
+}
+
+export default function user(props:UserProps) {
+    const { dataUsers } = props;
+    const router = useRouter()
+
+    console.log(dataUsers);
     return (
         <Layout pageTitle="User Page">
-             <h1 className={"title-homepage"}>User Page</h1>
+            {dataUsers.map((user) => {
+                return (
+                    <div key={user.id} onClick={() => router.push(`/users/${user.id}`)} className={styles.card}>
+                    <p>{user.name}</p>
+                    <p>{user.email}</p>
+                    </div>
+                )
+            })}
         </Layout>
     )
+}
+
+export async function getStaticProps() {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const dataUsers = await res.json();
+    return {
+        props: {
+            dataUsers,
+        },
+    };
 }
